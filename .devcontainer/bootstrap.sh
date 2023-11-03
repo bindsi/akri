bd49db81491ec8f2e2640f0da1ed9e55786247ce
Marcel Bindseil <marcelbindseil@gmail.com>
2023-11-03 14:58:51 +0100

    added cluster creation

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,6 +1,9 @@
 ./build/setup.sh
 
 sudo curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.68.1
+source $HOME/.cargo/env
+rustup default 1.68.1
 
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create akri-cluster --servers 1
