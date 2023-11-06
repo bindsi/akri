5f8f4897be74d9b7d3db48a4ec204b03bbab6daa
Marcel Bindseil <marcelbindseil@gmail.com>
2023-11-06 14:37:01 +0000

    added devcontainer

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,13 +1,16 @@
 ./build/setup.sh
 
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.68.1
+sudo curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=1.70
 source $HOME/.cargo/env
-rustup default 1.68.1
+rustup default 1.70
 
 # Install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d cluster create akri-cluster --servers 1
-export AKRI_HELM_CRICTL_CONFIGURATION="--set kubernetesDistro=microk8s"
+export AKRI_HELM_CRICTL_CONFIGURATION="--set kubernetesDistro=k3s"
 helm repo add akri-helm-charts https://project-akri.github.io/akri/
-helm install akri akri-helm-charts/akri \
-  $AKRI_HELM_CRICTL_CONFIGURATION \
+helm install akri akri-helm-charts/akri $AKRI_HELM_CRICTL_CONFIGURATION
+
+# Install go-lang
+sudo apt-get update
+sudo apt-get install -y golang-go
